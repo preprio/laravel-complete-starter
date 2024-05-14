b3b6bd3f8319bdd6398a585354bed965e5df03e0
Tom Swinkels <tom@prepr.io>
2024-05-14 16:29:47 +0200

    From L10 to L11

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ABTestingController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\LivestreamController;
 use App\Http\Controllers\NavigationController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PersonalizationController;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('index');
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel.default({
+        laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
