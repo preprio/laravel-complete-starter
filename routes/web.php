4bc7a6df3548f6e27d793b95c65fd7f7e5921af2
Mandy <88187770+mlingham79@users.noreply.github.com>
2025-04-29 15:55:35 +0200

    Update web.php
    updated typo

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Route;
 
 // Include the PageController class
-use app\Http\Controllers\PageController;
+use App\Http\Controllers\PageController;
 
 // Add a route to the PageController function
 Route::get('/{param?}', [PageController::class, 'index']);
