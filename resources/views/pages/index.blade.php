a6d7bc27a48662d396997c9f0621c55d04a63ef8
Mandy <88187770+mlingham79@users.noreply.github.com>
2025-04-29 15:45:27 +0200

    Update index.blade.php
    Remove script and replace with placeholder

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,14 +2,8 @@
     <!-- import app.css -->
     @vite('resources/css/app.css')
 
-    <!-- Prepr Tracking Code -->
-    <script>
-        ! function (e, t, p, r, n, a, s) {
-        e[r] || ((n = e[r] = function () {
-        n.process ? n.process.apply(n, arguments) : n.queue.push(arguments)
-        }).queue = [], n.t = +new Date, (a = t.createElement(p)).async = 1, a.src = "https://cdn.tracking.prepr.io/js/prepr_v2.min.js?t=" + 864e5 * Math.ceil(new Date / 864e5), (s = t.getElementsByTagName(p)[0]).parentNode.insertBefore(a, s))
-        }(window, document, "script", "prepr"), prepr("init", "ac_02b4a145e69be5488eb0e3abeada8ae5a2f88b0af09b33d442533b59e3bf4cd8"), prepr("event", "pageload");
-    </script>
+    <!-- Your Prepr Tracking Code -->
+  
     <meta property='prepr:id' content="{{ data_get($page,'_id') }}"/>
     <x-navbar/>
 
